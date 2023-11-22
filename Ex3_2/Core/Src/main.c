ab1017b3b5a9102df339b7871d9374f7495d2890
elecage <elecage@gmail.com>
2023-11-22 12:43:10 +0900

    전역변수 volatile 추가

--- Ex3_2/Core/Src/main.c
+++ Ex3_2/Core/Src/main.c
@@ -43,7 +43,7 @@
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
-uint16_t count = 0;
+volatile uint16_t count = 0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
